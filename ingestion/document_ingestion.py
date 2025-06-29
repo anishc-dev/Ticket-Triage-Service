ad9b7f0f735ed51df440ae35bbb42b2b1b5578cc
anishc-dev <mail2anishnair@gmail.com>
2025-06-29 21:28:24 +0530

    Ingesting documents from docs.netskope.com to vector db(chromadb)

---
+++ ingestion/document_ingestion.py
@@ -0,0 +1,36 @@
+"""
+    Ingest documents from a sitemap and store them in a vector database
+"""
+
+import chromadb
+from ingestion.sitemap_parser import SitemapParser
+
+
+class DocumentIngestion:
+    def __init__(self):
+        # Initalising the chromadb for vector database
+        self.client = chromadb.PersistentClient(path="./chroma_db")
+        # Creating a collection for the documents
+        self.collection = self.client.get_collection("netskope_docs")
+        self.sitemap = "https://docs.netskope.com/docs-sitemap.xml"
+    
+    def ingest_from_sitemap(self):
+        """
+            Ingest documents from a sitemap and store them in a vector database
+        """
+        # Parse the sitemap and get the pages
+        pages = SitemapParser(self.sitemap).get_pages()
+        # iterate over the pages and add them to the chromadb collection
+        for page in pages:
+            self.ingest_page(page['url'], page['content'])
+
+    def ingest_page(self, url, content):
+        """
+            Ingest a single page into the chromadb collection
+        """
+        self.collection.add(
+            documents=[content],
+            ids=[url]
+        )
+
+
---
+++ ingestion/sitemap_parser.py
@@ -0,0 +1,25 @@
+import requests
+import xml.etree.ElementTree as ET #used element tree for efficiency, used lxml in different project
+
+class SitemapParser:
+    def __init__(self, sitemap_url):
+        self.url = sitemap_url
+
+    def get_pages(self):
+        """
+            parse the sitemap and return the pages
+        """
+        try:
+            resp = requests.get(self.url)
+            root = ET.fromstring(resp.content)
+            namespace = {'ns': 'http://www.sitemaps.org/schemas/sitemap/0.9'} #defined to get the namespace of the sitemap
+            pages = []
+            for url_elem in root.findall('.//ns:url', namespace):
+                loc_elem = url_elem.find('ns:loc', namespace)
+                if loc_elem is not None:
+                    url = loc_elem.text
+                    pages.append({'url': url, 'content': url})
+            return pages
+        except Exception as e:
+            print(f"Error parsing sitemap: {e}")
+            return []
