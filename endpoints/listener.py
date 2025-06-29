11b807767663d2135be1c3e0d62b59fb1a88d1c9
anishc-dev <mail2anishnair@gmail.com>
2025-06-29 23:36:48 +0530

    Getting the input question and responding from chroma db

--- endpoints/listener.py
+++ endpoints/listener.py
@@ -1,23 +1,34 @@
 from fastapi import APIRouter
 from pydantic import BaseModel, Field
 from typing import Optional, List
+from ingestion.document_ingestion import DocumentIngestion
 
-class Ticket(BaseModel):
-    ticket: Optional[int] = Field(default=None)
-    message: Optional[str] = Field(default=None)   
-    user_id: Optional[int] = Field(default=None)
+class QuestionRequest(BaseModel):
+    question: Optional[str] = Field(default="Test Question")
 
 router = APIRouter()
 
 @router.post("/respond")
-async def respond(ticket: Ticket):
+async def respond(input: QuestionRequest):
     """
         To respond to support engineer questions.
     """
-    return {"message": "Hello Respond"}
+    try:
+        question_text = input.question
+        ingestion = DocumentIngestion()
+        
+        result = ingestion.collection.query(
+            query_texts=[question_text],
+            include=["documents"],
+            n_results=10
+        )
+        documents = result.get("documents", [])
+        return {"documents": documents}     
+    except Exception as e:
+        return {"message": f"Error: {str(e)}"}
 
 @router.post("/classify")
-async def classify(ticket: Ticket):
+async def classify(question: QuestionRequest):
     """
         To classify the ticket into a category.
     """
